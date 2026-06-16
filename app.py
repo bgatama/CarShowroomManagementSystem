98c32950b5c0da41ae859299f0cd7ae919d7ced6
bgatama <bryanfrancisgatama@gmail.com>
2026-06-16 22:05:08 +0300

    Added basic flask app running and part of the login page

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("hello world")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def the_start():
+    return 'beginning to the car showroom management system'
+
+if __name__ == "__main__":
+    app.run()
