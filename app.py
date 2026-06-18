7f1bb7ed223dcddb18f042e467e46c114b1cc9f1
bgatama <bryanfrancisgatama@gmail.com>
2026-06-18 16:14:34 +0300

    Flask receives the form from login page, using POST request

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 
 @app.route('/')
-def the_start():
+def template_to_render(): #render the login page
     return render_template('login.html')
 
+@app.route('/login', methods=['POST'])
+def login_to_db():
+    # Logic to connect to the database and verify user credentials
+    username = request.form['username']
+    password = request.form['password']
+
+    if username and password:
+        ... # Code to connect to the database and verify credentials
+
+
+    
 if __name__ == "__main__":
     app.run()
